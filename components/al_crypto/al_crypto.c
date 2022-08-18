4be2dd407ed17dd47c146a31fb75487458e7f1a7
dastu08 <37212582+dastu08@users.noreply.github.com>
2022-08-18 11:50:00 +0200

    Documentation, clean up, helper functions
    - Add function documentation in header file and for local functions in source.
    - Renamed function that will be exported to contain component name.
    - Created iv generation function.
    - Removed unnecessary and outcommended stuff.

--- components/al_crypto/al_crypto.c
+++ components/al_crypto/al_crypto.c
@@ -12,11 +12,21 @@
 
 static const char* TAG = "al_crypto";
 
+// key string from config
 char key_string[65] = CONFIG_AES_256_KEY;
 
+// aes context needed for init
 mbedtls_aes_context ctx;
 
-// print the numbers 0 to length with spaces separated
+/** Print the numbers 0 to length with spaces separated
+
+**Parameters**
+    - length : up to which number is counted (length-1)
+
+**Description**
+    Outputs a nicely spaces index of decimal numbers with
+    2 digits.
+*/
 void print_index(int length) {
     printf("index: ");
     // print the index number above
@@ -26,7 +36,15 @@ void print_index(int length) {
     printf("\n");
 }
 
-// print length bytes in hex format
+/** Print bytes in hex format
+
+**Parameters**
+    - *bytes : array of bytes
+    - length : length of the byte array
+
+**Description**
+    Outputs a nicely spaces hex numbers of the bytes.
+*/
 void print_bytes(byte_t* bytes, int length) {
     byte_t byte;
     printf("bytes: ");
@@ -41,7 +59,15 @@ void print_bytes(byte_t* bytes, int length) {
     printf("\n");
 }
 
-// print length chars
+/** Print char string
+
+**Parameters**
+    - *chars : array of chars
+    - length : length of the char array
+
+**Description**
+    Outputs a nicely spaces chars.
+*/
 void print_chars(char* chars, int length) {
     printf("chars: ");
     for (int i = 0; i < length; i++) {
@@ -50,7 +76,18 @@ void print_chars(char* chars, int length) {
     printf("\n");
 }
 
-// copy len number of bytes from old to new
+/** Copy a byte array
+
+**Parameters**
+    - *old : byte array containing the data
+    - *new : byte array where the data is copied into
+    - len : length of byte arrays
+
+**Description**
+    In a loop copy the values from one byte array to the
+    other one. Only the first len bytes are copied regard
+    less of the length of the arrays.
+*/
 void copy_bytes(byte_t* old, byte_t* new, int len) {
     for (int i = 0; i < len; ++i) {
         new[i] = old[i];
@@ -59,6 +96,23 @@ void copy_bytes(byte_t* old, byte_t* new, int len) {
 
 // convert the hex string in chars into length bytes
 // 2 hex digits make one byte
+
+/** Convert a hex string of chars into an array of bytes
+
+**Parameters**
+    - *chars : hex string that will be converted
+    - *bytes : array of bytes for the converted data
+    - length :
+        length of the byte array, half length of the char
+        array
+
+**Requirements**
+    Char array must have an even number of hex digits.
+
+**Description**
+    Read in the hex string and convert two consecutive
+    hex digits into one byte.
+*/
 void convert_hex2bytes(char* chars, byte_t* bytes, int length) {
     char buff[5];
     buff[0] = '0';
@@ -67,15 +121,15 @@ void convert_hex2bytes(char* chars, byte_t* bytes, int length) {
     for (int i = 0; i < length; i++) {
         buff[2] = chars[2 * i];
         buff[3] = chars[2 * i + 1];
-        // printf("i = %d : ", i);
         bytes[i] = strtol(buff, NULL, 16);
+        // printf("i = %d : ", i);
         // printf("buff: %c%c%c%c, value: 0x%X\n",
         //    buff[0], buff[1], buff[2], buff[3], bytes[i]);
         // print_chars(buff, 4);
     }
 }
 
-void padding_message(byte_t* message, int max_len) {
+void al_crypto_padding_message(byte_t* message, int max_len) {
     size_t len = strlen((char*)message);
     for (size_t i = len; i < max_len; ++i) {
         message[i] = 0x0;
@@ -84,12 +138,16 @@ void padding_message(byte_t* message, int max_len) {
 
 void al_crypto_init() {
     byte_t key_bytes[32];
+    int seed = (int)get_seed();
 
     convert_hex2bytes(key_string, key_bytes, 32);
     ESP_LOGV(TAG, "key string: %s", key_string);
     ESP_LOGV(TAG, "key");
     print_index(32);
     print_bytes(key_bytes, 32);
+
+    ESP_LOGV(TAG, "seed: %d", seed);
+    srand(seed);
 
     mbedtls_aes_init(&ctx);
     mbedtls_aes_setkey_enc(&ctx, key_bytes, 256);
@@ -98,32 +156,38 @@ void al_crypto_init() {
     ESP_LOGI(TAG, "init finished");
 }
 
+void al_crypto_generate_iv(byte_t* iv, int len) {
+    for (int i = 0; i < len; ++i) {
+        iv[i] = rand() % 256;
+    }
+}
+
 void al_crypto_encrypt(byte_t* plaintext,
                        int len,
                        byte_t* iv,
                        byte_t* ciphertext) {
     ESP_LOGD(TAG, "encrypting");
     byte_t iv2[16];
+    // need to create a copy to prevent destruction.
     copy_bytes(iv, iv2, 16);
-    // byte_t in[len];
-    // byte_t out[len];
 
     ESP_LOGV(TAG, "iv");
     print_index(16);
     print_bytes(iv, 16);
     print_chars((char*)iv, 16);
 
-    // convert the char to byte_t
-    // for (int i = 0; (i < len); i++) {
-    //     in[i] = plaintext[i];
-    // }
-    // in[16] = '\0';
     ESP_LOGV(TAG, "plaintext");
     print_index(len);
     print_bytes(plaintext, len);
     print_chars((char*)plaintext, len);
-    mbedtls_aes_crypt_cbc(&ctx, ESP_AES_ENCRYPT, len, iv2, plaintext, ciphertext);
-    // out[16] = '\0';
+
+    mbedtls_aes_crypt_cbc(&ctx,
+                          ESP_AES_ENCRYPT,
+                          len,
+                          iv2,
+                          plaintext,
+                          ciphertext);
+
     ESP_LOGV(TAG, "iv");
     print_index(16);
     print_bytes(iv, 16);
@@ -141,30 +205,29 @@ void al_crypto_decrypt(byte_t* ciphertext,
                        byte_t* plaintext) {
     ESP_LOGD(TAG, "decrypting");
     byte_t iv2[16];
+    // need to create a copy to prevent destruction.
     copy_bytes(iv, iv2, 16);
-    // byte_t in[16];
-    // byte_t out[16];
+
     ESP_LOGV(TAG, "iv");
     print_index(16);
     print_bytes(iv, 16);
     print_chars((char*)iv, 16);
 
-    // for (int i = 0; (i < len) && (i < 16); i++) {
-    //     in[i] = ciphertext[i];
-    // }
-    // in[16] = '\0';
     ESP_LOGV(TAG, "ciphertext:");
     print_index(len);
     print_bytes(ciphertext, len);
     print_chars((char*)ciphertext, len);
-    mbedtls_aes_crypt_cbc(&ctx, ESP_AES_DECRYPT, len, iv2, ciphertext, plaintext);
-    // out[16] = '\0';
+
+    mbedtls_aes_crypt_cbc(&ctx,
+                          ESP_AES_DECRYPT,
+                          len,
+                          iv2,
+                          ciphertext,
+                          plaintext);
+
     ESP_LOGV(TAG, "plaintext:");
     print_index(len);
     print_bytes(plaintext, len);
     print_chars((char*)plaintext, len);
 }
 
-// void myencrypt(int key, char* msg, char* out) {
-//     out = msg;
-// }
--- components/al_crypto/al_crypto.h
+++ components/al_crypto/al_crypto.h
@@ -6,17 +6,100 @@
 
 typedef unsigned char byte_t;
 
-void padding_message(byte_t* message, int max_len);
+/** Padding of a message with zeros
 
+**Parameters**
+    - *message : byte array of the message
+    - max_length : total length in memory of the byte array
+
+**Requirements**
+    Message musst be null terminated so strlen on it does
+    not return an error.
+
+**Description**
+    Determine the length of the message in the array. Then
+    fill up the remaining space with zeros until the end.
+*/
+void al_crypto_padding_message(byte_t* message, int max_len);
+
+/** Initialize the al_crypto component
+
+**Requirements**
+    A key string of length 64 hex digits must be specified
+    in the config.
+
+**Description**
+    Get a seed for the random generator and initialize it.
+    Convert the key hex string into bytes and initialize
+    the mbedtls aes stuff with the key.
+*/
 void al_crypto_init();
+
+/** Generate an initialization vector (IV)
+**Parameters**
+    - *iv : byte array to hold the iv in it
+    - len : number of bytes of the iv
+
+**Requirements**
+    Initialize the random number generator with srand().
+
+**Description**
+    Generate len number of bytes (values 0-255) and store
+    them in the iv byte array.
+*/
+void al_crypto_generate_iv(byte_t* iv, int len);
+
+/** Encrypt the plain text with AES-CBC mode
+**Parameters**
+    - *plaintext : byte array of the plain text
+    - len : length of the plaintext in bytes
+    - *iv : initilization vector (length 16 bytes)
+    - *ciphertext : byte array for the cipher text
+
+**Requirements**
+    Component al_cypto must be initialized with 
+    `al_crypto_init()`. The iv must be 16 bytes long and
+    randomly generated. Use `al_crypto_generate_iv()`.
+    Plaintext and ciphertext must have the same length
+    and this must be a multiple of 16 bytes. Plaintext must
+    also be padded before. Use 
+    `al_crypto_padding_message()`.
+
+**Description**
+    Copyt the iv because it will be modified during the
+    encryption. Encrypt the plaintext in AES-CBC mode with
+    the specified initialization vector (IV). The key was 
+    set during initialization.
+*/
 void al_crypto_encrypt(byte_t* plaintext,
                        int len,
                        byte_t* iv,
                        byte_t* ciphertext);
+
+
+/* Decrypt the cipher text with AES-CBC mode
+**Parameters**
+    - *ciphertext : byte array of the cipher text
+    - len : length of the ciphertext in bytes
+    - *iv : initilization vector (length 16 bytes)
+    - *plaintext : byte array for the plain text
+
+**Requirements**
+    Component al_cypto must be initialized with 
+    `al_crypto_init()`. The iv must be 16 bytes long and
+    received with the cipher text. Plaintext and ciphertext 
+    must have the same length and this must be a multiple of
+    16 bytes.
+
+**Description**
+    Copyt the iv because it will be modified during the
+    encryption. Decrypt the ciphertext in AES-CBC mode with
+    the specified initialization vector (IV). The key was 
+    set during initialization.
+*/                     
 void al_crypto_decrypt(byte_t* ciphertext,
                        int len,
                        byte_t* iv,
                        byte_t* plaintext);
-// void myencrypt(int key, char* msg, char* out);
-
+                       
 #endif
